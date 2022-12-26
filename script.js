b0ab5b2a3dc1e1deb10394063fd74ed58af92f02
jefersonviana <jefersonv28@gmail.com>
2022-12-26 18:22:26 -0300

    requisito 2 concluído

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 const body = document.querySelector('body');
 const container = document.createElement('div');
 container.id = 'meme-image-container';
+const containerImage = document.createElement('div');
+containerImage.id = 'meme-image';
 const input = document.createElement('input');
 input.id = 'text-input';
+input.type = 'text';
+const inputImage = document.createElement('input');
+inputImage.id = 'meme-insert';
+inputImage.type = 'file';
+body.appendChild(inputImage);
 body.appendChild(container);
 body.appendChild(input);
 const paragraph = document.createElement('p');
 paragraph.id = 'meme-text';
 container.appendChild(paragraph);
+container.appendChild(containerImage);
 
 const texto = () => {
   paragraph.innerText = input.value;
 };
 
 input.addEventListener('keyup', texto)
+
+inputImage.addEventListener('change', (event) => {
+  const inputTarget = event.target;
+  console.log(inputTarget);
+  const file = inputTarget.files[0];
+  console.log(file);
+  if (file) {
+    const reader = new FileReader();
+    console.log(reader);
+    reader.addEventListener('load', (event) => {
+      const readerTarget = event.target;
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.className = 'meme';
+      containerImage.innerHTML = '';
+      containerImage.appendChild(img);
+    });
+    reader.readAsDataURL(file);
+  }
+});
